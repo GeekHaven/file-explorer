e0c10a7ee5a28a6a80d0ac264eb2edea2ba08442
untruly_noxius <chetangarg365@gmail.com>
2020-10-09 16:55:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## file-explorer
+
+A simple file-explorer using  [Electron](https://github.com/electron/electron)
+
+
+
+Sketch:
+
+![](images/sketch.png)
---
+++ app.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    console.log("app started");
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.webContents.loadFile('index.html');
+})
+
