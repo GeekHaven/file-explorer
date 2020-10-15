ca6bcbd62b4fdaea64295bbc745ef7e789f115f4
Manish Kumar <manish15052000@gmail.com>
2020-10-15 22:44:52 +0530

    added dev tools button

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const {app, BrowserWindow} = require('electron');
- 
+const { app, BrowserWindow } = require("electron");
+
 let mainWindow = null;
 
-
-
-
-app.on('ready', () => {
-    console.log("app started");
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true
-        }
-    });
-    mainWindow.webContents.loadFile('index.html');
-})
-
+app.on("ready", () => {
+  console.log("app started");
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+  mainWindow.webContents.loadFile("index.html");
+});
--- renderer.js
+++ renderer.js
@@ -1,60 +1,59 @@
-let os = require('os');
+let os = require("os");
 const fs = require("fs");
-const path = require('path');
-const remote = require('electron').remote;
+const path = require("path");
+const remote = require("electron").remote;
 const win = remote.getCurrentWindow();
 
 document.onreadystatechange = (event) => {
-    if (document.readyState == "complete") {
-        handleWindowControls();
-    }
+  if (document.readyState == "complete") {
+    handleWindowControls();
+  }
 };
 
 window.onbeforeunload = (event) => {
-    win.removeAllListeners();
-}
+  win.removeAllListeners();
+};
 
 function handleWindowControls() {
-    document.getElementById('min-button').addEventListener("click", event => {
-        win.minimize();
-    });
+  document.getElementById("min-button").addEventListener("click", (event) => {
+    win.minimize();
+  });
 
-    document.getElementById('close-button').addEventListener("click", event => {
-        win.close();
-    });
+  document.getElementById("close-button").addEventListener("click", (event) => {
+    win.close();
+  });
+
+  document.getElementById("devbtn").addEventListener("click", (e) => {
+    win.webContents.openDevTools();
+  });
 }
 
 const printdirectory = () => {
-    dir_home = os.homedir();
-    console.log(dir_home);
-    return dir_home;
-}
+  dir_home = os.homedir();
+  console.log(dir_home);
+  return dir_home;
+};
 
 const getFilesandFolder = () => {
-    let home = printdirectory();
-    homenames = fs.readdirSync(home);
-    homenames.forEach(file => {
-
-        var filePath = path.join(home, file);
-        var stat = fs.statSync(filePath);
-        if (stat.isFile()) {
-            console.log('The is a File' + file);
-            var node = document.createElement('LI');
-            var textnode = document.createTextNode(file);
-            node.appendChild(textnode);
-            document.getElementById("myList").appendChild(node);
-        } else if (stat.isDirectory()) {
-            console.log('The is a Directory' + file);
-            var node = document.createElement('LI');
-            var textnode = document.createTextNode(file);
-            node.appendChild(textnode);
-            document.getElementById("myList").appendChild(node);
-        }
-
-
-
-    });
-}
-
+  let home = printdirectory();
+  homenames = fs.readdirSync(home);
+  homenames.forEach((file) => {
+    var filePath = path.join(home, file);
+    var stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      console.log("The is a File" + file);
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(file);
+      node.appendChild(textnode);
+      document.getElementById("myList").appendChild(node);
+    } else if (stat.isDirectory()) {
+      console.log("The is a Directory" + file);
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(file);
+      node.appendChild(textnode);
+      document.getElementById("myList").appendChild(node);
+    }
+  });
+};
 
 getFilesandFolder();
