31d4ccd9f0ea5a4dc5fcaeca93b1eeb37539c47d
prince-7 <amanvijaykumarsingh7792@gmail.com>
2020-10-21 00:02:52 +0530

    Directory Seaching Functionality Implemented

--- renderer.js
+++ renderer.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const remote = require("electron").remote;
 const win = remote.getCurrentWindow();
-const { shell } = require('electron')
+const { shell } = require('electron');
+const app = require('electron').remote.app;
 
 var node = document.getElementById("myList");
 
@@ -44,29 +45,26 @@ const printdirectory = () => {
 const getFilesandFolder = (folderPath) => {
   var homenames = fs.readdirSync(folderPath);
     node.innerHTML='';
-
-homenames.forEach((file) => {
-  var filePath = path.join(folderPath, file);
-  var stat = fs.statSync(filePath);
-  if (stat.isFile()) {
-    console.log("The is a File" + file);
-    var toAddnode = document.createElement("LI");
-    toAddnode.onclick = function() {openFile(filePath)};
-    var textnode = document.createTextNode(file);
-    toAddnode.appendChild(textnode);
-    
-    node.appendChild(toAddnode);
-  } else if (stat.isDirectory()) {
-
-    console.log("The is a Directory" + file);
-    var toAddnode = document.createElement("LI");
-    toAddnode.onclick=function() {getFilesandFolder(filePath)};
-    var textnode = document.createTextNode(file);
-    toAddnode.appendChild(textnode);
-    node.appendChild(toAddnode);
-
-  }
-});
+    homenames.forEach((file) => {
+      var filePath = path.join(folderPath, file);
+      var stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        console.log("The is a File" + file);
+        var toAddnode = document.createElement("LI");
+        toAddnode.onclick = function() {openFile(filePath)};
+        var textnode = document.createTextNode(file);
+        toAddnode.appendChild(textnode);
+        node.appendChild(toAddnode);
+      }
+      else if (stat.isDirectory()) {
+        console.log("The is a Directory" + file);
+        var toAddnode = document.createElement("LI");
+        toAddnode.onclick=function() {getFilesandFolder(filePath)};
+        var textnode = document.createTextNode(file);
+        toAddnode.appendChild(textnode);
+        node.appendChild(toAddnode);
+      }
+    });
 };
 
 function openFile(filePath){
@@ -74,3 +72,13 @@ function openFile(filePath){
 }
 
 getFilesandFolder(printdirectory());
+
+function showDir(){
+  var dirPath = document.getElementById('directory').value;
+  try{
+    getFilesandFolder(dirPath);
+  }
+  catch (e) {
+     alert("No Such directory found");
+   }
+}
