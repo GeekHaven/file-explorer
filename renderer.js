ee42b75272f00721e49ea1f0b318d28a74be22a0
prince-7 <amanvijaykumarsingh7792@gmail.com>
2020-10-21 13:35:09 +0530

    Updates

--- renderer.js
+++ renderer.js
@@ -43,28 +43,38 @@ const printdirectory = () => {
 };
 
 const getFilesandFolder = (folderPath) => {
+  try{
   var homenames = fs.readdirSync(folderPath);
-    node.innerHTML='';
-    homenames.forEach((file) => {
+  node.innerHTML='';
+  homenames.forEach((file) => {
+    try{
       var filePath = path.join(folderPath, file);
-      var stat = fs.statSync(filePath);
-      if (stat.isFile()) {
-        console.log("The is a File" + file);
-        var toAddnode = document.createElement("LI");
-        toAddnode.onclick = function() {openFile(filePath)};
-        var textnode = document.createTextNode(file);
-        toAddnode.appendChild(textnode);
-        node.appendChild(toAddnode);
-      }
-      else if (stat.isDirectory()) {
-        console.log("The is a Directory" + file);
-        var toAddnode = document.createElement("LI");
-        toAddnode.onclick=function() {getFilesandFolder(filePath)};
-        var textnode = document.createTextNode(file);
-        toAddnode.appendChild(textnode);
-        node.appendChild(toAddnode);
-      }
-    });
+    var stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      console.log("The is a File" + file);
+      var toAddnode = document.createElement("LI");
+      toAddnode.onclick = function() {openFile(filePath)};
+      var textnode = document.createTextNode(file);
+      toAddnode.appendChild(textnode);
+      node.appendChild(toAddnode);
+    }
+    else if (stat.isDirectory()) {
+      console.log("The is a Directory" + file);
+      var toAddnode = document.createElement("LI");
+      toAddnode.onclick=function() {getFilesandFolder(filePath)};
+      var textnode = document.createTextNode(file);
+      toAddnode.appendChild(textnode);
+      node.appendChild(toAddnode);
+    }
+    }
+    catch(err){
+      console.log(err);
+    }
+  });
+}
+catch(err){
+  alert(err);
+}
 };
 
 function openFile(filePath){
@@ -75,10 +85,5 @@ getFilesandFolder(printdirectory());
 
 function showDir(){
   var dirPath = document.getElementById('directory').value;
-  try{
     getFilesandFolder(dirPath);
-  }
-  catch (e) {
-     alert("No Such directory found");
-   }
 }
